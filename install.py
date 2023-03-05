45e7379ca96c6b68af64ddba85583e2ff3f76039
thitsa <tam@myaing.com>
2023-03-05 11:44:40 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,6 @@
 import wget
 import argparse
 
-wget.download("https://www.dropbox.com/s/gtr5rjb3x589lyl/credentials.json?dl=1")
-
 parser = argparse.ArgumentParser(description='Install Neuron Guard and Neuron Apps.')
 parser.add_argument('--archive', nargs='?', help='URL for archive.txt')
 
@@ -10,3 +8,8 @@ args = parser.parse_args()
 
 if (args.archive):
     wget.download(args.archive)
+    exit
+
+// Basic install of Neuron Guard
+    
+wget.download("https://www.dropbox.com/s/gtr5rjb3x589lyl/credentials.json?dl=1")
