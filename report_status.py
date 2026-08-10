730f30daf935f827904b5644d229485bcc66a425
thitsa <tam@myaing.com>
2026-08-09 21:32:54 -0400

    CLeaned up

--- report_status.py
+++ report_status.py
@@ -1,221 +1,140 @@
-# report_status.py v1.0
-# MUMTAZ (c) Neuronic 2023
+# report_status.py v1.1
+# Neuronic 2025
 
-import os, sys, time, subprocess, webbrowser
-import gspread
-from datetime import datetime
-import logging
-import audit_setting
+import os
+import sys
+import time
 import socket
-
 import ctypes
-from ctypes.wintypes import RGB
-from ctypes import byref, c_int, windll, wintypes
-from os import getenv, getcwd
+from ctypes import windll
+from datetime import datetime
+
+import gspread
 import keyboard
-################################################################################################
-############# Make connection to the google sheet #############
-# Credentials [Keys etc]
-credPath = ""
-credFileName = credPath + "credentials.json"
-mServiceAccount = gspread.service_account(filename=credFileName)
-################################################################################################
-# Google sheet Id
-mGoogleSheetId = audit_setting.sheetID # FOR TESTING
-################################################################################################
-# Connect the sheet based on sheet id
-mGoogleSheet = mServiceAccount.open_by_key(mGoogleSheetId)
-################################################################################################
-############# Define Sheet name to be edited #############
-if(audit_setting.sheetName == ""):
-    sheetName = mGoogleSheet.worksheets()[0].title
-else:
-    sheetName = audit_setting.sheetName
-################################################################################################
-# Acees google sheet from the mentioned sheet name
-mSelectedWorkSheet = mGoogleSheet.worksheet(sheetName)
-################################################################################################
-############# Define the installed path for the app #############
-# FOR EXE/CHROME
-appDefaultPath = audit_setting.appEXEPath
-################################################################################################
-# crash log For updating the google audit sheet
-crash_file = os.path.join(audit_setting.appPath, audit_setting.appName, "crash.log")
-################################################################################################
-## For CMD Console
-# get the handle to the console bar
-consoleBarHandler = ctypes.windll.kernel32.GetConsoleWindow()
-# hide the CMD Console
-windll.user32.ShowWindow(consoleBarHandler, 0)
-####################################################################################################
-# Message
-print ("Updating google sheet periodically...")
-print (sheetName)
+import audit_setting
 
-################################################################################################
-# Update sheet delay
-updateCounter = 0
-#maxIteration = 50
-################################################################################################
-# Assign google sheet row and col for that machine name
-cellNum = mSelectedWorkSheet.find(audit_setting.exhibitName)
-# Get machine postion from the google sheet
-machinIndex = cellNum.row
-################################################################################################
-### Refresh Counter ###
-isRefreshed = False
-totalStr = ''
-################################################################################################
-#### Store IP and Host of a machine and update to google sheet
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-print (host_name)
-print (host_ip)
-host_name_cell = 'B' + str(machinIndex)
-host_ip_cell = 'C' + str(machinIndex)
-print (host_name_cell)
+# ── Google Sheets connection ──────────────────────────────────────────────────
 
-mSelectedWorkSheet.update(host_name_cell, host_name)
-mSelectedWorkSheet.update(host_ip_cell, host_ip)
-################################################################################################
-# Open the crash log file and update to google sheet and clear the log file
-# update google sheet
-if os.path.exists(crash_file) :
-    with open(crash_file, 'r') as cLog:
-        total_crash = len(cLog.readlines())
-        # update google sheet
-        mSelectedWorkSheet.update(('F' + str(machinIndex)), total_crash)
-    # Write Crash Report
-    with open(crash_file, 'r') as cLog:
-        totalStr = ''
-        for line in cLog:
-            d = line.split(": App Restarted")[0].split(".")[0].split(":", 2)[2].split(' ')[1]
-            totalStr += d + ", "
-    with open(crash_file, 'w') as cLog:
-        print('Updated the crash report to google sheet for the day!!!')
-# update google sheet
-## Remove last , from the string
-totalStr = totalStr[:len(totalStr)-2]
-mSelectedWorkSheet.update(('G' + str(machinIndex)), totalStr)
-################################################################################################
-############# Getting the list of all running process from the task manager #############
-# Filter the app passed as param 'name'
+_here     = os.path.dirname(os.path.abspath(__file__))
+cred_file = os.path.join(_here, 'credentials.json')
+
 try:
-    def getTasks(name):
-        response = []
-        response = os.popen('tasklist').read().strip().split('\n')
-        #print(response[0].split('Mem Usage'))
-        # print ('# of tasks is %s' % (len(response)))
-        for i in range(len(response)):
-            s = response[i]
-            #print (name + " ---- ", response[i])
-            if name in response[i]:
-                # print ('%s in response[i]' %(name))
-                return response[i]
-        return []
-    if __name__ == '__main__':
-        '''
-        Timer to call the func (every 1 sec)
-        '''
-        while True:
-            appName = audit_setting.appEXEName.split('.exe')[0]
-            notResponding = 'Not Responding'
-            # res = getTasks(imgName)
-            res = getTasks((appName + '.exe'))
-            #print(res)
-            if not res:
-                ####################################################################
-                updateCounter = 0
-                ####################################################################
-                isChromeApp = appDefaultPath.find('Chrome')
-                if(isChromeApp != -1):
-                    appURL = audit_setting.appParams
-                    #######################################################################
-                    now = datetime.now()
-                    # convert to string
-                    time_str = now.strftime("%m/%d/%Y  %H:%M:%S")
-                    #######################################################################
-                else:
-                    #######################################################################
-                    now = datetime.now()
-                    # convert to string
-                    time_str = now.strftime("%m/%d/%Y  %H:%M:%S")
-                    #######################################################################
-                    time.sleep(1)
-            elif 'Not Responding' in res:
-                #######################################################################
-                #mSelectedWorkSheet.update(('D' + str(machinIndex)), "App Not Responding")
-                #######################################################################
-                now = datetime.now()
-                # convert to string
-                time_str = now.strftime("%m/%d/%Y  %H:%M:%S")
-                #######################################################################
-                time.sleep(1)
-            else:
-                if(machinIndex != -1):
-                    # terminating process
-                    #print('%s - Running ' % (appName))
-                    updateCounter = updateCounter+1
-                    if(updateCounter == 1):
-                        ############# Update Cell value #############
-                        now = datetime.now()
-                        ####################################################
-                        # convert to string
-                        date_time_str = now.strftime("%m/%d/%Y")
-                        time_str = now.strftime("%m/%d/%Y  %H:%M:%S")
-                        ####################################################
-                        # Update Date row to the current Audit
-                        ####################################################
-                        mSelectedWorkSheet.update(('D' + str(machinIndex)), "Ok")
-                        mSelectedWorkSheet.update(('E' + str(machinIndex)), time_str)
-                        print('Google Sheet Updated')
-                        ####################################################
-                    else:
-                        # check for status date column to be updated based on day change
-                        now = datetime.now()
-                        time_str = now.strftime("%H:%M:%S")
-                        if (time_str == '00:00:00'):
-                            ####################################################
-                            ## Update Host Name and Host IP to google sheet
-                            mSelectedWorkSheet.update(('B' + str(machinIndex)), host_name)
-                            mSelectedWorkSheet.update(('C' + str(machinIndex)), host_ip)
-                            ############# Update Cell value #############
-                            ####################################################
-                            # convert to string
-                            date_time_str = now.strftime("%m/%d/%Y")
-                            time_str = now.strftime("%m/%d/%Y  %H:%M:%S")
-                            ####################################################
-                            # Update Date row to the current Audit
-                            ####################################################
-                            mSelectedWorkSheet.update(('D' + str(machinIndex)), "Ok")
-                            mSelectedWorkSheet.update(('E' + str(machinIndex)), time_str)
-                            ####################################################
-                            # Open the crash log file and update to google sheet and clear the log file
-                            # update google sheet
-                            with open(crash_file, 'r') as cLog:
-                                total_crash = len(cLog.readlines())
-                                # update google sheet
-                                mSelectedWorkSheet.update(('F' + str(machinIndex)), total_crash)
-                            # Write Crash Report
-                            with open(crash_file, 'r') as cLog:
-                                totalStr = ''
-                                for line in cLog:
-                                    d = line.split(": App Restarted")[0].split(".")[0].split(":", 2)[2].split(' ')[1]
-                                    totalStr += d + ", "
-                            # update google sheet
-                            ## Remove last , from the string
-                            totalStr = totalStr[:len(totalStr)-2]
-                            mSelectedWorkSheet.update(('G' + str(machinIndex)), totalStr)
-                            with open(crash_file, 'w') as cLog:
-                                print('Updated the crash report to google sheet for the day!!!')
-                #else:
-                ########################################################################################################
-                # checking for Keybaord press
-                if keyboard.is_pressed("left windows") and keyboard.is_pressed("shift") and keyboard.is_pressed("d"):
-                    # Close CMD Console
-                    windll.user32.DestroyWindow(consoleBarHandler)
-                    break
-                ########################################################################################################
-                time.sleep(0)
-except KeyboardInterrupt:
-    print("Google sheet update script stopped")
+    service_account = gspread.service_account(filename=cred_file)
+    workbook        = service_account.open_by_key(audit_setting.sheetID)
+except Exception as e:
+    msg = str(e)
+    if 'invalid_grant' in msg or 'iat and exp' in msg or 'Token must be a short-lived' in msg:
+        print("ERROR: Google authentication failed because this machine's clock is out of sync.")
+        print("       Fix: open Settings > Time & Language > Date & Time")
+        print("         and set the correct date and time manually.")
+        print("       Then re-run this script.")
+    elif '403' in msg or 'does not have permission' in msg or isinstance(e, PermissionError):
+        print("ERROR: The service account does not have access to this Google Sheet.")
+        print("       Fix: open the sheet in Google Sheets, click Share, and add")
+        print("         editor@zapsheets-480701.iam.gserviceaccount.com  with Editor access.")
+    elif 'credentials.json' in msg or 'No such file' in msg:
+        print("ERROR: credentials.json not found. Make sure it is in the same folder as this script.")
+    else:
+        print("ERROR connecting to Google Sheets:", msg)
+    sys.exit(1)
+
+sheet_name = workbook.worksheets()[0].title if audit_setting.sheetName == "" else audit_setting.sheetName
+worksheet  = workbook.worksheet(sheet_name)
+
+# ── Locate this machine's row ─────────────────────────────────────────────────
+
+cell = worksheet.find(audit_setting.exhibitName)
+if cell is None:
+    print(f"ERROR: Exhibit '{audit_setting.exhibitName}' not found in sheet '{sheet_name}'.")
+    sys.exit(1)
+row = cell.row
+
+# ── Hide console window ───────────────────────────────────────────────────────
+
+console = ctypes.windll.kernel32.GetConsoleWindow()
+windll.user32.ShowWindow(console, 0)
+
+# ── Write hostname and IP on startup ─────────────────────────────────────────
+
+host_name = socket.gethostname()
+host_ip   = socket.gethostbyname(host_name)
+worksheet.update(f'B{row}', host_name)
+worksheet.update(f'C{row}', host_ip)
+print(f"Connected: {sheet_name} / {audit_setting.exhibitName} (row {row})")
+print(f"Host: {host_name}  IP: {host_ip}")
+
+# ── Crash log helpers ─────────────────────────────────────────────────────────
+
+crash_file = os.path.join(audit_setting.appPath, audit_setting.appName, 'crash.log')
+
+def report_crashes():
+    """Read crash log, write count and times to sheet, then clear the log."""
+    if not os.path.exists(crash_file):
+        return
+    with open(crash_file, 'r') as f:
+        lines = f.readlines()
+    times = []
+    for line in lines:
+        try:
+            t = line.split(': App Restarted')[0].split('.')[0].split(':', 2)[2].split(' ')[1]
+            times.append(t)
+        except IndexError:
+            pass
+    worksheet.update(f'F{row}', len(lines))
+    worksheet.update(f'G{row}', ', '.join(times))
+    open(crash_file, 'w').close()
+    print(f"Crash report updated: {len(lines)} restart(s)")
+
+report_crashes()
+
+# ── Process check ─────────────────────────────────────────────────────────────
+
+def is_running():
+    """Return the tasklist line for the app, or empty string if not found."""
+    for line in os.popen('tasklist').read().splitlines():
+        if audit_setting.appEXEName in line:
+            return line
+    return ''
+
+# ── Main monitoring loop ──────────────────────────────────────────────────────
+
+print("Monitoring...")
+
+update_counter = 0
+
+while True:
+    res = is_running()
+
+    if not res:
+        # App not running — reset so status is written again when it comes back
+        update_counter = 0
+        time.sleep(2)
+
+    elif 'Not Responding' in res:
+        time.sleep(2)
+
+    else:
+        update_counter += 1
+
+        if update_counter == 1:
+            # First detection after (re)start — write status to sheet
+            time_str = datetime.now().strftime("%m/%d/%Y  %H:%M:%S")
+            worksheet.update(f'D{row}', "Ok")
+            worksheet.update(f'E{row}', time_str)
+            print(f"Status updated: Ok  {time_str}")
+
+        elif datetime.now().strftime("%H:%M:%S") == '00:00:00':
+            # Daily midnight refresh
+            time_str = datetime.now().strftime("%m/%d/%Y  %H:%M:%S")
+            worksheet.update(f'B{row}', host_name)
+            worksheet.update(f'C{row}', host_ip)
+            worksheet.update(f'D{row}', "Ok")
+            worksheet.update(f'E{row}', time_str)
+            report_crashes()
+
+        # Win+Shift+D to exit
+        if keyboard.is_pressed("left windows") and keyboard.is_pressed("shift") and keyboard.is_pressed("d"):
+            windll.user32.DestroyWindow(console)
+            break
+
+        time.sleep(5)
