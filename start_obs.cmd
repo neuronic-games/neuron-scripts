4d3f2c3db23e24222ca6fda71810d897794916c4
thitsa <tam@myaing.com>
2026-08-14 20:32:53 -0400

    Clean obs start

--- start_obs.cmd
+++ start_obs.cmd
@@ -1,6 +1,11 @@
 :: Start OBS Studio with the virtual camera running
 :: (c) Neuronic 2026
 
+:: An unclean shutdown leaves the .sentinel folder behind, which forces
+:: OBS to show an "unclean shutdown" warning dialog on next launch (bad for
+:: an unattended kiosk). Delete it first so OBS starts silently.
+if exist "%appdata%\obs-studio\.sentinel" rmdir /s /q "%appdata%\obs-studio\.sentinel"
+
 :: OBS needs its working directory set to its own bin folder to find its
 :: plugins/data correctly when launched other than via its Start Menu shortcut.
 cd /d "C:\Program Files\obs-studio\bin\64bit"
